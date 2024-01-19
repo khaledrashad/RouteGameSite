35690a2d8f9b969a6b9665f390770f16a7c73889
amr rashad <khaledamr400@gmail.com>
2024-01-19 22:23:00 +0200

    navigating between genres using navbar

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-const gameShow = document.getElementById("gameShow") 
+const gameShow = document.getElementById("gameShow")
+const navBtns = document.getElementById("navBtns")
+let genre = "mmorpg"
 
 class Game {
     constructor(img, title, desc, genre, platform) {
@@ -17,34 +19,46 @@ const options = {
 	}
 };
 async function gameData(){
-    const response = await fetch('https://free-to-play-games-database.p.rapidapi.com/api/games?category=shooter',options);
+    const response = await fetch(`https://free-to-play-games-database.p.rapidapi.com/api/games?category=${genre}`,options);
     const result = await response.json();
     return result
 }
 
-gameData().then((data)=>{
-    console.log(data)
-    let gameList = ""
-    data.forEach(game => {
-        let temp = new Game (game.thumbnail,game.title,game.short_description,game.genre,game.platform)
-        let showCard = ` 
-        <div class="col-lg-3 col-md-6 col-sm-12" >
-            <div class="card text-center">
-                <div class="card-body overflow-hidden">
-                    <img src="${temp.img}" alt="" class="w-100 rounded-top-2">
-                    <div class="gameTitle d-flex justify-content-between my-3">
-                        <div id="gameInfo" class="align-self-center">${temp.title}</div>
-                        <div class="btn text-white titleBtn">Free</div>
+function renderGames (){
+    gameData().then((data)=>{
+        console.log(data)
+        let gameList = ""
+        data.forEach(game => {
+            let temp = new Game (game.thumbnail,game.title,game.short_description,game.genre,game.platform)
+            let showCard = ` 
+            <div class="col-lg-3 col-md-6 col-sm-12" >
+                <div class="card text-center">
+                    <div class="card-body overflow-hidden">
+                        <img src="${temp.img}" alt="" class="w-100 rounded-top-2">
+                        <div class="gameTitle d-flex justify-content-between my-3">
+                            <div id="gameInfo" class="align-self-center">${temp.title}</div>
+                            <div class="btn text-white titleBtn">Free</div>
+                        </div>
+                        <div id="gameDesc">${temp.desc}</div>
                     </div>
-                    <div id="gameDesc">${temp.desc}</div>
-                </div>
-                <div class="card-footer d-flex justify-content-between">
-                    <div id="gameCat">${temp.genre}</div>
-                    <div id="gameConsole">${temp.platform}</div>
-                </div>
-            </div>                   
-        </div>`
-        gameList += showCard
-    });
-    gameShow.innerHTML = gameList
+                    <div class="card-footer d-flex justify-content-between">
+                        <div id="gameCat">${temp.genre}</div>
+                        <div id="gameConsole">${temp.platform}</div>
+                    </div>
+                </div>                   
+            </div>`
+            gameList += showCard
+        });
+        gameShow.innerHTML = gameList
+    })
+}
+
+renderGames()
+
+
+
+navBtns.addEventListener("click", function(e){
+    e.preventDefault()
+    genre = e.target.getAttribute("id")
+    renderGames()
 })
