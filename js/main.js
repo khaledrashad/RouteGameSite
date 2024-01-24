2dadc895e55466fa614274b2325857f025bfb63b
amr rashad <khaledamr400@gmail.com>
2024-01-24 21:57:18 +0200

    modal js is done

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ const modalBtn = document.getElementById("modalBtn")
 const gamesDiv = document.getElementById("gamesDiv")
 const gameModal = document.getElementById("gameModal")
 let genre = "mmorpg"
+let gameId = ""
 
 class Game {
     constructor(img, title, desc, genre, platform,id) {
@@ -28,9 +29,17 @@ async function gameData() {
     return result
 }
 
-function showModal(){
+async function gameDescreption() {
+    const response = await fetch(`https://free-to-play-games-database.p.rapidapi.com/api/game?id=${gameId}`, options);
+    const result = await response.json();
+    return result
+}
+
+function showModal(e){
     gamesDiv.classList.add("d-none")
     gameModal.classList.add("d-block")
+    gameId = e.getAttribute("id")
+    renderModal()
 }
 
 function closeModal(){
@@ -47,7 +56,7 @@ function renderGames() {
             let showCard = ` 
             <div class="col-lg-3 col-md-6 col-sm-12" >
                 <div class="card text-center">
-                    <div class="card-body overflow-hidden" id=${cardInfo.id} onclick="showModal()">
+                    <div class="card-body overflow-hidden" id=${cardInfo.id} onclick="showModal(this)">
                         <img src="${cardInfo.img}" alt="" class="w-100 rounded-top-2">
                         <div class="gameTitle d-flex justify-content-between my-3">
                             <div id="gameInfo" class="align-self-center">${cardInfo.title}</div>
@@ -69,6 +78,19 @@ function renderGames() {
 
 renderGames()
 
+function renderModal(){
+    gameDescreption().then((data)=>{
+        console.log(data)
+        document.getElementById("gameTitle").innerHTML = `${data.title}`
+        document.getElementById("gameCat").innerHTML = `${data.genre}`
+        document.getElementById("gamePlat").innerHTML = `${data.platform}`
+        document.getElementById("gameStat").innerHTML = `${data.status}`
+        document.getElementById("gameParagraph").innerHTML = `${data.description}`
+        document.getElementById("gameTitle").innerHTML = `${data.title}`
+        document.getElementById("modalImg").setAttribute("src",`${data.thumbnail}`)
+        document.getElementById("gameBtn").setAttribute("href",`${data.game_url}`)
+    })
+}
 
 
 navBtns.addEventListener("click", function (e) {
